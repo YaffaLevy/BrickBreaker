1eb9950f844e82abdc2c82dd9c4af952e27b62c0
Yaffa Levy <yaffajlevy@gmail.com>
2024-11-05 17:40:31 -0500

    model classes

---
+++ src/main/java/levy/brickBreaker/Brick.java
@@ -0,0 +1,25 @@
+package levy.brickBreaker;
+
+public class Brick {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private boolean destroyed;
+
+    public Brick(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.destroyed = false;
+    }
+
+    // Getters and setters only
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    public boolean isDestroyed() { return destroyed; }
+    public void setDestroyed(boolean destroyed) { this.destroyed = destroyed; }
+}
---
+++ src/main/java/levy/brickBreaker/Paddle.java
@@ -0,0 +1,38 @@
+package levy.brickBreaker;
+
+public class Paddle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int speed;
+
+    public Paddle(int x, int y, int width, int height, int speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+
+    public int getX() { return x; }
+    public void setX(int x) { this.x = x; }
+    public int getY() { return y; }
+    public void setY(int y) { this.y = y; }
+    public int getWidth() { return width; }
+    public void setWidth(int width) { this.width = width; }
+    public int getHeight() { return height; }
+    public void setHeight(int height) { this.height = height; }
+    public int getSpeed() { return speed; }
+    public void setSpeed(int speed) { this.speed = speed; }
+
+
+    public void moveLeft() {
+        x -= speed;
+    }
+
+    public void moveRight() {
+        x += speed;
+    }
+}
