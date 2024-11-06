bee60cc6293b385b6b49f1eb23730323d6fa2aca
Yaffa Levy <yaffajlevy@gmail.com>
2024-11-06 11:45:14 -0500

    some updates

--- src/main/java/levy/brickBreaker/GameModel.java
+++ src/main/java/levy/brickBreaker/GameModel.java
@@ -5,10 +5,10 @@ import java.util.List;
 public class GameModel {
     private Ball ball;
     private Paddle paddle;
-    private List<Brick> bricks;
+    private List<Bricks> bricks;
     private List<Wall> walls;
 
-    public GameModel(Ball ball, Paddle paddle, List<Brick> bricks, List<Wall> walls) {
+    public GameModel(Ball ball, Paddle paddle, List<Bricks> bricks, List<Wall> walls) {
         this.ball = ball;
         this.paddle = paddle;
         this.bricks = bricks;
@@ -16,7 +16,7 @@ public class GameModel {
     }
     public Ball getBall() { return ball; }
     public Paddle getPaddle() { return paddle; }
-    public List<Brick> getBricks() { return bricks; }
+    public List<Bricks> getBricks() { return bricks; }
     public List<Wall> getWalls() { return walls; }
 
     public void updateBallPosition(double x, double y) {
