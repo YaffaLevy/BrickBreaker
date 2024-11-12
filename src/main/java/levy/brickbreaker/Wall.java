90294ecf32a2f6bd4e145ba49f0262fd68a1c79b
Yaffa Levy <yaffajlevy@gmail.com>
2024-11-12 17:11:18 -0500

    fixed wall class

--- src/main/java/levy/brickbreaker/Wall.java
+++ src/main/java/levy/brickbreaker/Wall.java
@@ -1,16 +1,28 @@
 package levy.brickbreaker;
 
 public class Wall {
-    private int x;
-    private int y;
+
     private int width;
     private int height;
 
-    public Wall(int x, int y, int width, int height) {
+    public Wall( int width, int height) {
 
-        this.x = x;
-        this.y = y;
         this.width = width;
+        this.height = height;
+    }
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
         this.height = height;
     }
 }
