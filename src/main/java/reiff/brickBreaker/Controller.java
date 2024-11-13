961ef6e5fd3730bb8091e704455a6116a14af251
SheyReiff <sheyreiff@gmail.com>
2024-11-12 23:01:50 -0500

    checkstyles

--- src/main/java/reiff/brickBreaker/Controller.java
+++ src/main/java/reiff/brickBreaker/Controller.java
@@ -69,9 +69,9 @@ public class Controller {
     }
 
     private void checkPaddleCollision() {
-        if (ball.getY() + ball.getDiameter() >= paddle.getY() &&
-                ball.getX() >= paddle.getX() &&
-                ball.getX() <= paddle.getX() + paddle.getWidth()) {
+        if (ball.getY() + ball.getDiameter() >= paddle.getY()
+                && ball.getX() >= paddle.getX()
+                && ball.getX() <= paddle.getX() + paddle.getWidth()) {
 
             double paddleCenterX = paddle.getX() + paddle.getWidth() / 2;
             double edgeZoneWidth = paddle.getWidth() * 0.1;
@@ -81,16 +81,16 @@ public class Controller {
 
             double ballAngle = ball.getDirectionDegrees();
 
-            if (ball.getX() >= leftEdgeEnd &&
-                    ball.getX() <= paddleCenterX &&
-                    (ballAngle > 270 || ballAngle < 90)) {
+            if (ball.getX() >= leftEdgeEnd
+                    && ball.getX() <= paddleCenterX
+                    && (ballAngle > 270 || ballAngle < 90)) {
                 ball.setDirectionDegrees((180 + ball.getDirectionDegrees()) % 360);
-            } else if (ball.getX() <= rightEdgeStart &&
-                    ball.getX() >= paddleCenterX &&
-                    (ballAngle > 90 && ballAngle < 270)) {
+            } else if (ball.getX() <= rightEdgeStart
+                    && ball.getX() >= paddleCenterX
+                    && (ballAngle > 90 && ballAngle < 270)) {
                 ball.setDirectionDegrees((180 + ball.getDirectionDegrees()) % 360);
-            } else if ((ball.getX() <= leftEdgeEnd && ballAngle > 270) ||
-                    (ball.getX() >= rightEdgeStart && ballAngle < 90)) {
+            } else if ((ball.getX() <= leftEdgeEnd && ballAngle > 270)
+                    || (ball.getX() >= rightEdgeStart && ballAngle < 90)) {
                 ball.setDirectionDegrees(-ball.getDirectionDegrees());
             } else if (ball.getX() >= paddleCenterX - 2 && ball.getX() <= paddleCenterX + 2) {
                 ball.setDirectionDegrees(270);
@@ -103,11 +103,11 @@ public class Controller {
 
     private void checkBrickCollisions() {
         for (Bricks brick : bricks) {
-            if (!brick.isDestroyed() &&
-                    ball.getX() + ball.getDiameter() >= brick.getX() &&
-                    ball.getX() <= brick.getX() + brick.getWidth() &&
-                    ball.getY() + ball.getDiameter() >= brick.getY() &&
-                    ball.getY() <= brick.getY() + brick.getHeight()) {
+            if (!brick.isDestroyed()
+                    && ball.getX() + ball.getDiameter() >= brick.getX()
+                    && ball.getX() <= brick.getX() + brick.getWidth()
+                    && ball.getY() + ball.getDiameter() >= brick.getY()
+                    && ball.getY() <= brick.getY() + brick.getHeight()) {
                 brick.setDestroyed(true);
                 ball.setDirectionDegrees(-ball.getDirectionDegrees());
                 break;
